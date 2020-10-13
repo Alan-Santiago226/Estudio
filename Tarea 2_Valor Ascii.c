0fdf77c1e18861c6b8c411e535a2069022d93dcf
Alan-Santiago226 <72393801+Alan-Santiago226@users.noreply.github.com>
2020-10-13 18:39:33 -0500

    Add files via upload

---
+++ Tarea 2_Valor Ascii.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	char caracter = '^';
+	int valor_ascii = (int)caracter;
+	printf("el valor ascii es %d de %c \n", valor_ascii, caracter);
+	if(valor_ascii >= 48 && valor_ascii <= 57){
+		printf("Es digito");
+	}
+	else{
+		if(valor_ascii >= 65 && valor_ascii <= 90){
+		printf("El valor %c es mayuscula", caracter);
+		}
+		else{
+			if(valor_ascii >= 97 && valor_ascii <= 122){
+				printf ("El valor %c es minuscula", caracter);
+			}
+			else {
+				printf("El valor %c es un caracter especial\n", caracter);
+			}
+			
+		}
+				
+		
+	}
+	system("pause");
+}
